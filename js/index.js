1a9c2a9e6c177467e215a2ba024363f7d7de2e16
Andresss21 <andrs27999@gmail.com>
2021-09-01 16:22:31 -0600

    awdawdwadaw

--- js/index.js
+++ js/index.js
@@ -1,61 +1,8 @@
-const setup = () => {
-    const getTheme = () => {
-        if (window.localStorage.getItem('dark')) {
-        return JSON.parse(window.localStorage.getItem('dark'))
-        }
-        return !!window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').matches
-    }
+// grab everything we need
+const btn = document.querySelector(".mobile-menu-button");
+const sidebar = document.querySelector(".sidebar");
 
-    const setTheme = (value) => {
-        window.localStorage.setItem('dark', value)
-    }
-
-    return {
-        loading: true,
-        isDark: getTheme(),
-        toggleTheme() {
-        this.isDark = !this.isDark
-        setTheme(this.isDark)
-        },
-        setLightTheme() {
-        this.isDark = false
-        setTheme(this.isDark)
-        },
-        setDarkTheme() {
-        this.isDark = true
-        setTheme(this.isDark)
-        },
-        watchScreen() {
-        if (window.innerWidth <= 1024) {
-            this.isSidebarOpen = false
-        } else if (window.innerWidth >= 1024) {
-            this.isSidebarOpen = true
-        }
-        },
-        isSidebarOpen: window.innerWidth >= 1024 ? true : false,
-        toggleSidbarMenu() {
-        this.isSidebarOpen = !this.isSidebarOpen
-        },
-        isNotificationsPanelOpen: false,
-        openNotificationsPanel() {
-        this.isNotificationsPanelOpen = true
-        this.$nextTick(() => {
-            this.$refs.notificationsPanel.focus()
-        })
-        },
-        isSettingsPanelOpen: false,
-        openSettingsPanel() {
-        this.isSettingsPanelOpen = true
-        this.$nextTick(() => {
-            this.$refs.settingsPanel.focus()
-        })
-        },
-        isSearchPanelOpen: false,
-        openSearchPanel() {
-        this.isSearchPanelOpen = true
-        this.$nextTick(() => {
-            this.$refs.searchInput.focus()
-        })
-        },
-    }
-    }
+// add our event listener for the click
+btn.addEventListener("click", () => {
+  sidebar.classList.toggle("-translate-x-full");
+});
